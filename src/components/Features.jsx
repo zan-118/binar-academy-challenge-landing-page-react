b1404c82872e4bc4cbffa8d666d57944f07fb87e
zan-118 <fauzanabdulbasith@gmail.com>
2023-09-11 13:57:42 +0700

    update content

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -15,25 +15,25 @@ export default function Features() {
         alignItems: "center",
       }}
     >
-      <div>
-        <div className="py-8 text-center">
+      <div className="lg:ms-80">
+        <div className="py-8 ">
           <h3 className="text-2xl">What's So Special?</h3>
           <h3 className="uppercase text-5xl">Features</h3>
         </div>
         <div className="w-3/4">
-          <ul className="steps  ">
-            <li className="step ">
-              <h2 className="text-warning uppercase">Traditional Games</h2>
-              <p className="text-start">
+          <ul className="steps steps-vertical ">
+            <li className="step w-96 ">
+              <h2 className="text-warning uppercase mx-6">Traditional Games</h2>
+              <span className="w-80 mx-10  text-justify">
                 if you miss your chilhood, we provide many traditional games
                 here.
-              </p>
+              </span>
             </li>
-            <li className="step">
-              <h2 className="text-warning uppercase">Game Suit</h2>
+            <li className="step w-96">
+              <h2 className="text-warning uppercase mx-6">Game Suit</h2>
             </li>
-            <li className="step ">
-              <h2 className="text-warning uppercase">TBD</h2>
+            <li className="step w-96">
+              <h2 className="text-warning uppercase mx-6">TBD</h2>
             </li>
           </ul>
         </div>
